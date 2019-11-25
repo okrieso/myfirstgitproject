37ddfd23291d943b7f568e06b1de0175d5627692
okrieso <okrieso@gmail.com>
2019-11-25 17:05:02 +0300

    function isLeapYear

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -1,13 +1,40 @@
 ﻿unit DateTimeMethods;
+interface
+///возращает истину если год високосный 
+function IsLeapYear(year: integer): boolean;
+///находит количество дней в месяце
+function DaysInMonth(month, year: integer): integer;
 
-function IsLeapYear(year: integer): boolean := false;
-function DaysInMonth(month, day: integer): integer := 0;
 function LaterInDay(p1, p2: DateTime): DateTime := p1;
+
 function LaterInYear(p1, p2: DateTime): DateTime := p1;
+
 function DaysInYear(year: integer): integer := 0;
+
 function DaysInYearRange(year1, year2: integer): integer := 0;
+
 function SecondsInHours(hours: integer): integer := 0;
 
-begin
+implementation
 
+function isLeapYear(year: integer): boolean;
+begin
+  result:=false;
+  if (year mod 4) = 0 then
+    result:=true;
+end;
+
+function DaysInMonth(month, year: integer): integer;
+begin
+  case month of
+    1,3,5,7,8,10,12: result:=31;
+    4,6,9,11: result:=30;
+    2: begin
+    result:=28;
+    if isLeapYear(year) then
+      result:=29;
+    end;
+  end;
+end;
+begin  
 end.
